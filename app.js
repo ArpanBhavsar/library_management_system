2637544c45e504307f3c6d70de6b8bc93fba0510
Arpan Bhavsar <arpanbhavsar.160410116019@gmail.com>
2024-06-20 05:36:57 -0700

    Add EJS files and routes to perform CRUD operarion on books collection.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 const mongodb = require("mongodb");
+const methodOverride = require("method-override");
 require("dotenv").config();
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
 
 const port = process.env.PORT || 3000;
 const mongoURI = process.env.MONGO_URI;
@@ -26,6 +28,85 @@ mongodb.MongoClient.connect(mongoURI)
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// Route to get all books and render on /books
+app.get("/books", (req, res) => {
+  db.collection("books").find().toArray()
+    .then(books => {
+      res.render("index", { books }); // Renders the index.ejs template from the views folder
+    })
+    .catch(err => {
+      res.status(500).json({ error: "An error occurred while retrieving books", details: err });
+    });
+});
+
+// Show form to create a new book
+app.get("/books/new", (req, res) => {
+  res.render("add_book"); // Renders the new.ejs template from the views folder
+});
+
+// Create a new book
+app.post("/books", (req, res) => {
+  const book = {
+    title: req.body.title,
+    author: req.body.author,
+    publishedDate: new Date(req.body.publishedDate) // Ensure this is a Date object
+  };
+
+  db.collection("books").insertOne(book)
+    .then(result => res.redirect("/books"))
+    .catch(err => res.status(500).json({ error: "An error occurred while creating the book", details: err }));
+});
+
+// Show form to edit a book
+app.get("/books/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const objectId = new mongodb.ObjectId(id); // Correct way to create ObjectId
+
+  db.collection("books").findOne({ _id: objectId })
+    .then(book => {
+      if (!book) {
+        return res.status(404).json({ error: "Book not found" });
+      }
+      res.render("edit_book", { book }); // Renders the edit.ejs template from the views folder
+    })
+    .catch(err => res.status(500).json({ error: "An error occurred while retrieving the book", details: err }));
+});
+
+// Update a book
+app.put("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const objectId = new mongodb.ObjectId(id); // Correct way to create ObjectId
+  const updatedBook = {
+    title: req.body.title,
+    author: req.body.author,
+    publishedDate: new Date(req.body.publishedDate) // Ensure this is a Date object
+  };
+
+  db.collection("books").updateOne({ _id: objectId }, { $set: updatedBook })
+    .then(result => {
+      if (result.matchedCount === 0) {
+        return res.status(404).json({ error: "Book not found" });
+      }
+      res.redirect("/books");
+    })
+    .catch(err => res.status(500).json({ error: "An error occurred while updating the book", details: err }));
+});
+
+// Delete a book
+app.delete("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const objectId = new mongodb.ObjectId(id); // Correct way to create ObjectId
+
+  db.collection("books").deleteOne({ _id: objectId })
+    .then(result => {
+      if (result.deletedCount === 0) {
+        return res.status(404).json({ error: "Book not found" });
+      }
+      res.redirect("/books");
+    })
+    .catch(err => res.status(500).json({ error: "An error occurred while deleting the book", details: err }));
 });
 
 app.listen(port, () => {
